62bf6c19afe1cebb96bae51ae182de74d0a3a348
TimurSennikov <timursennikov1@gmail.com>
2024-09-28 23:00:59 +0300

    fix adding to bal

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const tg = window.Telegram.WebApp;
 window.onload = () => {
     setInterval(() => {
         tg.CloudStorage.getItem("skrypakmb_bal", (err, data) => {
-            if(err){alert("wtf?");} // watafak?
+            if(err){} // watafak?
             else{document.getElementById("score").innerHTML = data;}
         });
     }, 1000);
 
     document.getElementById("clickArea").onclick = () => {
         tg.CloudStorage.getItem("skrypakmb_bal", (err, data) => {
-            tg.CloudStorage.setItem("skrypakmb_bal", data + 1);
+            tg.CloudStorage.setItem("skrypakmb_bal", parseInt(data, 10) + 1);
         });
     };
 };
